bfb5d5818d36e28223eaf473e6e50fa531383ac2
Saeedullah15 <saeedullahazim65@gmail.com>
2024-09-05 14:18:32 +0600

    layout changing functionality implemented

--- src/pages/all-queries/AllQueries.jsx
+++ src/pages/all-queries/AllQueries.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
+import { TfiLayoutColumn2Alt, TfiLayoutColumn3Alt, TfiLayoutColumn4Alt } from "react-icons/tfi";
 import { useLoaderData } from 'react-router-dom';
 import AllQueriesEachCard from './AllQueriesEachCard';
 
@@ -7,6 +8,7 @@ const AllQueries = () => {
     const data = useLoaderData();
     const [queries, setQueries] = useState(data);
     const [searchText, setSearchText] = useState('');
+    const [layout, setLayout] = useState("grid-cols-3");
     console.log(data);
 
     const handleSearch = () => {
@@ -21,20 +23,35 @@ const AllQueries = () => {
     }
 
     return (
-        <div className='mt-10'>
-            <div className='flex justify-center items-center space-x-2'>
-                <input type="text"
-                    onChange={(e) => {
-                        setSearchText(e.target.value);
-                        handleSearch(); // for real-time search
-                    }}
-                    placeholder="Search By Product Name"
-                    className="input input-bordered w-full max-w-xs"
-                />
-                <button onClick={handleSearch} className="btn btn-primary">Search</button>
+        <div className='mt-10 max-w-6xl mx-auto'>
+            <div className='flex justify-between'>
+                <div className='flex space-x-2 w-1/2'>
+                    <input type="text"
+                        onChange={(e) => {
+                            setSearchText(e.target.value);
+                            handleSearch(); // for real-time search
+                        }}
+                        placeholder="Search By Product Name"
+                        className="input input-bordered w-full"
+                    />
+                    <button onClick={handleSearch} className="btn btn-primary">Search</button>
+                </div>
+
+                {/* layout button */}
+                <div className='space-x-2'>
+                    <button onClick={() => setLayout("grid-cols-2")} className='btn btn-outline btn-success'>
+                        <TfiLayoutColumn2Alt className='text-xl' />
+                    </button>
+                    <button onClick={() => setLayout("grid-cols-3")} className='btn btn-outline btn-success'>
+                        <TfiLayoutColumn3Alt className='text-xl' />
+                    </button>
+                    <button onClick={() => setLayout("grid-cols-4")} className='btn btn-outline btn-success'>
+                        <TfiLayoutColumn4Alt className='text-xl' />
+                    </button>
+                </div>
             </div>
 
-            <section className='grid grid-cols-3 mt-10 max-w-6xl mx-auto gap-10'>
+            <section className={`grid ${layout} mt-10 gap-10`}>
                 {
                     queries.map(eachData => <AllQueriesEachCard
                         key={eachData._id}
